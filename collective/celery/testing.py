e417a76860743539bb51065d79356834498f19d6
Eric BREHAULT <ebrehault@gmail.com>
2014-11-05 14:01:46 +0100

    set CELERY_ALWAYS_EAGER to True

--- collective/celery/testing.py
+++ collective/celery/testing.py
@@ -1,9 +1,11 @@
 import os
 from plone.testing import Layer
+from collective.celery.utils import getCelery
 
 class CeleryTestLayer(Layer):
 
     def setUp(self):
-        os.environ['CELERY_ALWAYS_EAGER'] = 'True'
+        getCelery().conf['CELERY_ALWAYS_EAGER'] = 'True'
+        
 
 CELERY = CeleryTestLayer()
