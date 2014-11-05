21c97ee9ffd3172a1d8660ac52e8e9a175fbad97
Eric BREHAULT <ebrehault@gmail.com>
2014-11-05 16:17:40 +0100

    set result backend

--- collective/celery/testing.py
+++ collective/celery/testing.py
@@ -1,11 +1,16 @@
-import os
 from plone.testing import Layer
-from collective.celery.utils import getCelery
+from collective.celery.utils import getCelery, _getCelery
+
 
 class CeleryTestLayer(Layer):
 
     def setUp(self):
         getCelery().conf['CELERY_ALWAYS_EAGER'] = 'True'
+        # use in-memory sqlite
+        getCelery().conf['BROKER_URL'] = "sqla+sqlite://"
+        getCelery().conf['CELERY_RESULT_BACKEND'] = "db+sqlite://"
+        # refresh cached properties
+        _getCelery()
         
 
 CELERY = CeleryTestLayer()
